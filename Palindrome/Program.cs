2ea3ed1739f5321ba1d394650319c3bce80c0551
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-30 01:13:17 +0100

    Palindrome sem função local

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Palindrome
 {
@@ -6,7 +7,43 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string s in args)
+            {
+                Console.WriteLine($"{s} -> {Palindromo(s)}");
+            }
+        }
+
+        public static bool Palindromo(string palavra)
+        {
+            if (palavra == null)
+            {
+                throw new ArgumentNullException(nameof(palavra));
+            }
+
+            if(palavra.Length < 2)
+            { 
+                return true;
+            }
+
+            Queue<char> queue = new Queue<char>();
+            Stack<char> stack = new Stack<char>(); 
+            
+            foreach (char c in palavra)
+            {
+                queue.Enqueue(c);
+                stack.Push(c);
+            }
+
+            while (queue.Count > 0)
+            {
+                if(queue.Dequeue() != stack.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return true;      
+            
         }
     }
 }
