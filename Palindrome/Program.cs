01aa2e2328cd67f37c6a9a8ef3c96ae207b3c655
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-30 01:17:15 +0100

    Palindromo com função local

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -25,25 +25,28 @@ namespace Palindrome
                 return true;
             }
 
-            Queue<char> queue = new Queue<char>();
-            Stack<char> stack = new Stack<char>(); 
-            
-            foreach (char c in palavra)
+            bool IsPalindromo(string s)
             {
-                queue.Enqueue(c);
-                stack.Push(c);
-            }
-
-            while (queue.Count > 0)
-            {
-                if(queue.Dequeue() != stack.Pop())
+                Queue<char> queue = new Queue<char>();
+                Stack<char> stack = new Stack<char>(); 
+                
+                foreach (char c in s)
                 {
-                    return false;
+                    queue.Enqueue(c);
+                    stack.Push(c);
                 }
+
+                while (queue.Count > 0)
+                {
+                    if(queue.Dequeue() != stack.Pop())
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
 
-            return true;      
-            
+            return IsPalindromo(palavra);          
         }
     }
 }
